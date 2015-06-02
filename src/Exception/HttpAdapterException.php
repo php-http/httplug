c92df82db7b0b15bcc8aa17ee459a10889b608d3
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-06-02 10:52:46 +0200

    Moves exceptions back to the adapter package

--- src/Exception/HttpAdapterException.php
+++ src/Exception/HttpAdapterException.php
@@ -18,47 +18,94 @@ use Psr\Http\Message\ResponseInterface;
 /**
  * @author GeLo <geloen.eric@gmail.com>
  */
-interface HttpAdapterException extends Exception
+class HttpAdapterException extends \Exception implements Exception
 {
+    /**
+     * @var RequestInterface|null
+     */
+    private $request;
+
+    /**
+     * @var ResponseInterface|null
+     */
+    private $response;
+
     /**
      * Returns the request
      *
      * @return RequestInterface|null
      */
-    public function getRequest();
+    public function getRequest()
+    {
+        return $this->request;
+    }
 
     /**
      * Checks if there is a request
      *
      * @return boolean
      */
-    public function hasRequest();
+    public function hasRequest()
+    {
+        return isset($this->request);
+    }
 
     /**
      * Sets the request
      *
      * @param RequestInterface|null $request
      */
-    public function setRequest(RequestInterface $request = null);
+    public function setRequest(RequestInterface $request = null)
+    {
+        $this->request = $request;
+    }
 
     /**
      * Returns the response
      *
      * @return ResponseInterface|null
      */
-    public function getResponse();
+    public function getResponse()
+    {
+        return $this->response;
+    }
 
     /**
      * Checks if there is a response
      *
      * @return boolean
      */
-    public function hasResponse();
+    public function hasResponse()
+    {
+        return isset($this->response);
+    }
 
     /**
      * Sets the response
      *
      * @param ResponseInterface|null $response
      */
-    public function setResponse(ResponseInterface $response = null);
+    public function setResponse(ResponseInterface $response = null)
+    {
+        $this->response = $response;
+    }
+
+    /**
+     * @param string          $uri
+     * @param string          $adapterName
+     * @param \Exception|null $previous
+     */
+    public static function cannotFetchUri($uri, $adapterName, \Exception $previous = null)
+    {
+        $message = sprintf(
+            'An error occurred when fetching the URI "%s" with the adapter "%s" ("%s").',
+            $uri,
+            $adapterName,
+            isset($previous) ? $previous->getMessage() : ''
+        );
+
+        $code = isset($previous) ? $previous->getCode() : 0;
+
+        return new self($message, $code, $previous);
+    }
 }
