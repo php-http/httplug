adf45ace1f35d75f6efe474633ed7f1f0eb6524f
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-10-20 21:00:19 +0200

    Minor changes

--- src/Exception.php
+++ src/Exception.php
@@ -9,5 +9,4 @@ namespace Http\Client;
  */
 interface Exception
 {
-
 }
--- src/Exception/BatchException.php
+++ src/Exception/BatchException.php
@@ -4,14 +4,13 @@ namespace Http\Client\Exception;
 
 use Http\Client\BatchResult;
 use Http\Client\Exception;
-use Psr\Http\Message\RequestInterface;
 
 /**
  * This exception is thrown when a batch of requests led to at least one failure.
  *
  * It holds the response/exception pairs and gives access to a BatchResult with the successful responses.
  *
-*@author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
  */
 final class BatchException extends \RuntimeException implements Exception
 {
