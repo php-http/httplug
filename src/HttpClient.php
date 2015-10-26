fbc001c5ca8b12c6ffeb2b3d564b10b673e4ebb3
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2015-10-26 09:49:46 +0100

    Remove multiple requests from HTTP Client
    Remove Batch related stuff (result, exception)
    
    Remove batch tests

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -25,22 +25,4 @@ interface HttpClient
      * @throws Exception
      */
     public function sendRequest(RequestInterface $request);
-
-    /**
-     * Sends several PSR-7 requests.
-     *
-     * If the client is able to, these requests should be sent in parallel. Otherwise they will be sent sequentially.
-     * Either way, the caller may not rely on them being executed in any particular order.
-     *
-     * If one or more requests led to an exception, the BatchException is thrown. The BatchException gives access to the
-     * BatchResult that contains responses for successful calls and exceptions for unsuccessful calls.
-     *
-     * @param RequestInterface[] $requests
-     *
-     * @return BatchResult If all requests where successful.
-     *
-     * @throws Exception      On general setup problems.
-     * @throws BatchException If one or more requests led to exceptions.
-     */
-    public function sendRequests(array $requests);
 }
