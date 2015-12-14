24697ac0d90eba050bb173732fef0099a89cfea4
sagikazarmark <mark.sagikazar@gmail.com>
2015-12-14 09:21:00 -0500

    Applied fixes from StyleCI

--- spec/Exception/HttpExceptionSpec.php
+++ spec/Exception/HttpExceptionSpec.php
@@ -2,33 +2,33 @@
 
 namespace spec\Http\Client\Exception;
 
+use PhpSpec\ObjectBehavior;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
-use PhpSpec\ObjectBehavior;
 
 class HttpExceptionSpec extends ObjectBehavior
 {
-    function let(RequestInterface $request, ResponseInterface $response)
+    public function let(RequestInterface $request, ResponseInterface $response)
     {
         $this->beConstructedWith('message', $request, $response);
     }
 
-    function it_is_initializable()
+    public function it_is_initializable()
     {
         $this->shouldHaveType('Http\Client\Exception\HttpException');
     }
 
-    function it_is_a_request_exception()
+    public function it_is_a_request_exception()
     {
         $this->shouldHaveType('Http\Client\Exception\RequestException');
     }
 
-    function it_has_a_response(ResponseInterface $response)
+    public function it_has_a_response(ResponseInterface $response)
     {
         $this->getResponse()->shouldReturn($response);
     }
 
-    function it_creates_an_http_exception(RequestInterface $request, ResponseInterface $response)
+    public function it_creates_an_http_exception(RequestInterface $request, ResponseInterface $response)
     {
         $request->getRequestTarget()->willReturn('/uri');
         $request->getMethod()->willReturn('GET');
