35b49da592cb0a0fc5d1c3318ea64d08a9752f13
Atef Ben Ali <atefBB@users.noreply.github.com>
2018-08-04 06:22:26 +0100

    Update HttpClient.php

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -3,7 +3,6 @@
 namespace Http\Client;
 
 use Psr\Http\Message\RequestInterface;
-// use Psr\Http\Message\ResponseInterface;
 
 /**
  * Sends a PSR-7 Request and returns a PSR-7 response.
@@ -19,7 +18,7 @@ interface HttpClient
      *
      * @param RequestInterface $request
      *
-     * @return ResponseInterface
+     * @return Psr\Http\Message\ResponseInterface
      *
      * @throws \Http\Client\Exception If an error happens during processing the request.
      * @throws \Exception             If processing the request is impossible (eg. bad configuration).
