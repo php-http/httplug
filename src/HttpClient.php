d7f1b189a8f15c2d52a6d1d2cff7b57169973119
Mponos George <gmponos@gmail.com>
2019-03-05 09:37:27 +0200

    Remove exception docblock (#151)

--- src/HttpClient.php
+++ src/HttpClient.php
@@ -3,23 +3,13 @@
 namespace Http\Client;
 
 use Psr\Http\Client\ClientInterface;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 
 /**
- * Sends a PSR-7 Request and returns a PSR-7 response.
+ * {@inheritdoc}
  *
- * @author GeLo <geloen.eric@gmail.com>
- * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
- * @author David Buchmann <mail@davidbu.ch>
+ * Provide the Httplug HttpClient interface for BC.
+ * You should typehint Psr\Http\Client\ClientInterface in new code
  */
 interface HttpClient extends ClientInterface
 {
-    /**
-     * Sends a PSR-7 request.
-     *
-     * @throws \Http\Client\Exception If an error happens during processing the request.
-     * @throws \Exception             If processing the request is impossible (eg. bad configuration).
-     */
-    public function sendRequest(RequestInterface $request): ResponseInterface;
 }
